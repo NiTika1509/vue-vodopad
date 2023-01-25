746b76518dc1c2d13a8700ac8c1855fec827eb45
NiTika1509 <sicnax@gmail.com>
2023-01-25 15:39:37 +0300

    25.01.2023 Vodopad Hotel

--- src/views/Page/RoomsList.vue
+++ src/views/Page/RoomsList.vue
@@ -1,14 +1,46 @@
 <template>
-  {{ rooms }}
+
+
+  <div v-for="category of categories" :key="category" >
+    <h1>{{ category }}</h1>
+    <template v-for="room of items_room" :key="room">
+      <template v-if="room.category == category ">
+          {{ room }}
+      </template>
+    </template>
+  </div>
+
+
 </template>
 
 <script>
 export default {
   name: "RoomsList",
+  data(){
+    return{
+      categories: [],
+      items_room: []
+    }
+  },
   props:{
     rooms:{
-      type: Object
+      type: Array
     }
+  },
+  async mounted(){
+    await this.rooms.forEach( (floor) => {
+      floor.items.forEach((floorRoom)=>{
+        floorRoom.floorItems.forEach(room=>{
+          this.items_room.push(room)
+        })
+      })
+    })
+    await this.items_room.forEach( item =>{
+      if(this.categories.indexOf(item.category) === -1){
+        this.categories.push(item.category)
+      }
+    })
+
   }
 }
 </script>
